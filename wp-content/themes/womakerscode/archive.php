67da2108e256edad2a17cfff29a2c76b1dda938f
Eduardo Pittol <edpittol@gmail.com>
2016-03-18 19:33:58 -0300

    Archive template

---
+++ wp-content/themes/womakerscode/archive.php
@@ -0,0 +1,41 @@
+<?php get_header() ?>
+
+<main role="main">
+	<section>
+	
+		<h1>Posts de "<?php single_cat_title() ?>"</h1>
+
+		<?php while (have_posts()) : the_post(); ?>
+		
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h2 class="entry-title">
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+				
+				<?php 
+					if( has_post_thumbnail() ) :
+						the_post_thumbnail( 'full' );
+					endif;
+				?>
+				
+				<div class="entry-content"><?php the_content(); ?></div>
+		
+				<?php edit_post_link(); ?>
+			</article>
+		
+		<?php endwhile; ?>
+			
+		<?php 
+			the_posts_pagination( array(
+				'prev_text' => 'Previous page',
+				'next_text' => 'Next page',
+				'screen_reader_text' => ''
+			) );
+		?>
+
+	</section>
+</main>
+
+<?php get_sidebar() ?>
+
+<?php get_footer() ?>
