d5b99d2d37376b6f40441db86a8a815a223a3593
Eduardo Pittol <edpittol@gmail.com>
2016-03-18 19:22:20 -0300

    Create page and no sidebar page template

---
+++ wp-content/themes/womakerscode/no-sidebar.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Template Name: No sidebar
+ */
+
+get_header();
+
+?>
+
+<main role="main">
+	<section>
+
+		<?php while (have_posts()) : the_post(); ?>
+		
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h1 class-"entry-title>
+					<?php the_title(); ?>
+				</h1>
+				
+				<div class="entry-content"><?php the_content(); ?></div>
+		
+				<?php edit_post_link(); ?>
+			</article>
+		
+		<?php endwhile; ?>
+
+	</section>
+</main>
+
+<?php get_footer() ?>
