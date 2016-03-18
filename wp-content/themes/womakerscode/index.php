7ce129a25364c146a47d05cea4aa5ba965bbdd36
Eduardo Pittol <edpittol@gmail.com>
2016-03-18 15:56:12 -0300

    Add post pagination

--- wp-content/themes/womakerscode/index.php
+++ wp-content/themes/womakerscode/index.php
@@ -1,46 +1,36 @@
-<!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(); ?></title>
+<?php get_header() ?>
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+<main role="main">
+	<section>
 
-		<?php wp_head(); ?>
+		<?php while (have_posts()) : the_post(); ?>
+		
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h2>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+				
+				<?php 
+					if( has_post_thumbnail() ) :
+						the_post_thumbnail( 'full' );
+					endif;
+				?>
+		
+				<?php the_content(); ?>
+		
+				<?php edit_post_link(); ?>
+			</article>
+		
+		<?php endwhile; ?>
+		
+		<?php 
+			the_posts_pagination( array(
+				'prev_text' => 'Previous page',
+				'next_text' => 'Next page',
+			) );
+		?>
 
-	</head>
-	<body <?php body_class(); ?>>
+	</section>
+</main>
 
-		<div class="wrapper">
-
-			<header class="header clear" role="banner">
-				<h1><?php bloginfo('name'); ?></h1>
-			</header>
-
-			<main role="main">
-				<section>
-
-					<?php while (have_posts()) : the_post(); ?>
-
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<h2>
-								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-							</h2>
-					
-							<?php the_content(); ?>
-					
-							<?php edit_post_link(); ?>
-						</article>
-
-					<?php endwhile; ?>
-
-				</section>
-			</main>
-			
-		</div>
-
-		<?php wp_footer(); ?>
-	</body>
-</html>
+<?php get_footer() ?>
