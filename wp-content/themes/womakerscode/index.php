d9bd4e4ecb83ce32f1ae7a1674c075a1df3ede7f
Eduardo Pittol <edpittol@gmail.com>
2016-03-18 17:19:37 -0300

    Fix the post page link inner the loop

--- wp-content/themes/womakerscode/index.php
+++ wp-content/themes/womakerscode/index.php
@@ -20,16 +20,16 @@
 		
 				<?php edit_post_link(); ?>
 			</article>
-			
-			<?php 
-				the_posts_pagination( array(
-					'prev_text' => 'Previous page',
-					'next_text' => 'Next page',
-					'screen_reader_text' => ''
-				) );
-			?>
 		
 		<?php endwhile; ?>
+			
+		<?php 
+			the_posts_pagination( array(
+				'prev_text' => 'Previous page',
+				'next_text' => 'Next page',
+				'screen_reader_text' => ''
+			) );
+		?>
 
 	</section>
 </main>
