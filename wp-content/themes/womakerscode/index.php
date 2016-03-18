8b6c54a232a989ef040cf01b1b07ad17aad21702
Eduardo Pittol <edpittol@gmail.com>
2016-03-18 16:43:02 -0300

    Put all in a file

--- wp-content/themes/womakerscode/index.php
+++ wp-content/themes/womakerscode/index.php
@@ -1,36 +1,60 @@
-<?php get_header() ?>
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(); ?></title>
 
-<main role="main">
-	<section>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
-		<?php while (have_posts()) : the_post(); ?>
-		
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h2>
-				
-				<?php 
-					if( has_post_thumbnail() ) :
-						the_post_thumbnail( 'full' );
-					endif;
-				?>
-		
-				<?php the_content(); ?>
-		
-				<?php edit_post_link(); ?>
-			</article>
-		
-		<?php endwhile; ?>
-		
-		<?php 
-			the_posts_pagination( array(
-				'prev_text' => 'Previous page',
-				'next_text' => 'Next page',
-			) );
-		?>
+		<?php wp_head(); ?>
 
-	</section>
-</main>
+	</head>
+	<body <?php body_class(); ?>>
 
-<?php get_footer() ?>
+		<div class="wrapper">
+
+			<header class="header clear" role="banner">
+				<h1><?php bloginfo('name'); ?></h1>
+			</header>
+			
+			<main role="main">
+				<section>
+			
+					<?php while (have_posts()) : the_post(); ?>
+					
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<h2>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+							</h2>
+							
+							<?php 
+								if( has_post_thumbnail() ) :
+									the_post_thumbnail( 'full' );
+								endif;
+							?>
+					
+							<?php the_content(); ?>
+					
+							<?php edit_post_link(); ?>
+						</article>
+					
+					<?php endwhile; ?>
+					
+					<?php 
+						the_posts_pagination( array(
+							'prev_text' => 'Previous page',
+							'next_text' => 'Next page',
+							'screen_reader_text' => ''
+						) );
+					?>
+			
+				</section>
+			</main>
+			
+		</div>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
